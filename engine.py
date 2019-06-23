5871aa4ae46bc8c87ef49aa8c8033055110d1834
phxrocker <uopqs29@yahoo.com>
2019-06-23 13:33:55 -0700

    Part 2 Commit

--- engine.py
+++ engine.py
@@ -22,7 +22,7 @@ def main():
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+    libtcod.console_init_root(screen_width, screen_height, 'RoguelikeDev Tutorial', False)
 
     con = libtcod.console_new(screen_width, screen_height)
 
